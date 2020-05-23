1270b0214a10667431aad5ec955eb7754c80fbde
Natalie Wee <natalieweesh@gmail.com>
2020-05-23 11:04:59 -0400

    build number 5

---
+++ precache-manifest.a21a7f074653992c6cbe560ddeb3cc1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d5b0b3f0f705e70520fea2417c6d33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e48bb1e8574cca1999f6",
+    "url": "/static/css/main.54064acd.chunk.css"
+  },
+  {
+    "revision": "445601d56841a0c66c76",
+    "url": "/static/js/2.f9a0471b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9a0471b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48bb1e8574cca1999f6",
+    "url": "/static/js/main.d7d512b4.chunk.js"
+  },
+  {
+    "revision": "bb977efc2efb3e186080",
+    "url": "/static/js/runtime-main.300ba239.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tabata/precache-manifest.7abc8f9991769a11a9d71d57729b1049.js"
+  "/precache-manifest.a21a7f074653992c6cbe560ddeb3cc1e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tabata/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
