0830fb97b81af7a536208943894da0b5673c0c30
Natalie Wee <natalieweesh@gmail.com>
2020-05-23 00:40:07 -0400

    try another deploy

---
+++ precache-manifest.d545c56689435172920f5c4319a11db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7eedbc126b5c85ef15200b9d68d21ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "725960319abd4f831485",
+    "url": "/static/css/main.b0a672f4.chunk.css"
+  },
+  {
+    "revision": "445601d56841a0c66c76",
+    "url": "/static/js/2.f9a0471b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9a0471b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725960319abd4f831485",
+    "url": "/static/js/main.85794f96.chunk.js"
+  },
+  {
+    "revision": "bb977efc2efb3e186080",
+    "url": "/static/js/runtime-main.300ba239.js"
+  }
+]);
