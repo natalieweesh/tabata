cc5e2ea33bf5340c09eeb1e768990c06682b37f2
Natalie Wee <natalieweesh@gmail.com>
2020-05-23 22:11:40 -0400

    add FAQ section

--- src/App.js
+++ src/App.js
@@ -272,6 +272,7 @@ function App() {
   const [selectedMuscleGroups, setSelectedMuscleGroups] = useState(['arms', 'legs', 'butt', 'back'])
   const rounds = useRef(null)
   const [showPreview, setShowPreview] = useState(false);
+  const [showFAQ, setShowFAQ] = useState(false);
   const arrays = {}
   Object.keys(exercises).map((muscleGroup) => {
     let template = []
@@ -462,8 +463,14 @@ function App() {
         }} className={startedWorkout && !paused ? 'unpausedButton' : ''}>{!startedWorkout ? ("START") : (paused ? "UNPAUSE" : "PAUSE")}</button>
         {!startedWorkout && <button className="previewButton" onClick={() => {
           setShowPreview(!showPreview)
+          setShowFAQ(false)
           if (!showPreview) setTimeout(scrollDown, 200)
         }}>{showPreview ? 'Hide the exercises' : 'Preview the exercises'}</button>}
+        <button className="previewButton" onClick={() => {
+          setShowFAQ(!showFAQ);
+          setShowPreview(false)
+          if (!showFAQ) setTimeout(scrollDown, 200)
+        }}>{showFAQ ? 'Hide FAQ' : 'FAQ'}</button>
       </div>
       }
       
@@ -481,6 +488,14 @@ function App() {
           })}
         </div>
       }
+
+      {showFAQ && <div className="row previewRow faq">
+        <p><strong>What the squanch is tabata?</strong><br/>Tabata is a type of HIIT workout where you exercise for 20 seconds, rest for 10 seconds, then repeat until your heart falls out.</p>
+        
+        <p><strong>Who are your trainers?</strong><br/>Mr. Poopybutthole, Mr. Meeseeks, Tina Belcher, Spongebob Squarepants, and BMO are some of our distinguished trainers who will be working out with you today. Please note that their form while doing exercises is not always 100% on point so don't hurt yourself trying to copy them exactly.</p>
+        <p><strong>How does this all work?</strong><br/>Once you pick your preferred work time, rest time, and total workout duration, you can choose the muscle groups you want to focus on for your workout. Then the workout will cycle through each muscle group with a different random exercise each time until your total workout time runs out. Let's get schwifty!</p>
+        <p><strong>Who lives in a pineapple under the sea?</strong><br/>Spongebob Squarepants</p>
+      </div>}
     </div>
   )
 };
