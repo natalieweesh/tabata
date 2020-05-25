1f472f043f7d3831e474555466d8d5665af6fbc7
Natalie Wee <natalieweesh@gmail.com>
2020-05-24 21:06:59 -0400

    deploy 23 reorder the previews

--- precache-manifest.13566d14f7e427ae5346ca30ffb4c076.js
+++ precache-manifest.13566d14f7e427ae5346ca30ffb4c076.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8431b24e12c6f2c603176c50f7b6dd12",
+    "revision": "19d43d4be8269cf73faab2843ef9c4ee",
     "url": "/tabata/index.html"
   },
   {
-    "revision": "9315d2d843987646ea28",
+    "revision": "fec8322857a29757258d",
     "url": "/tabata/static/css/main.6593f588.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tabata/static/js/2.bd271a9c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9315d2d843987646ea28",
-    "url": "/tabata/static/js/main.72aad772.chunk.js"
+    "revision": "fec8322857a29757258d",
+    "url": "/tabata/static/js/main.4137c56e.chunk.js"
   },
   {
     "revision": "4469c1d1e27997529a6e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tabata/precache-manifest.13ae4be0a446d212e1f264201f1b294e.js"
+  "/tabata/precache-manifest.13566d14f7e427ae5346ca30ffb4c076.js"
 );
 
 self.addEventListener('message', (event) => {
