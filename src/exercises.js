877d94919772e3aa08276fad7c86ee37a520d9df
Natalie Wee <natalieweesh@gmail.com>
2020-05-25 18:11:49 -0400

    separate exercises file

---
+++ src/exercises.js
@@ -0,0 +1,501 @@
+export const exercises = {
+    arms: [
+      {
+        title: 'bicep curl',
+        img: 'https://media.giphy.com/media/UV4Yi70OhwQ6JwJo7m/giphy.gif',
+        character: 'meeseeks'
+      },
+      {
+        title: 'skull crusher',
+        img: 'https://media.giphy.com/media/MdjZSDee0C50ZuYlHa/giphy.gif',
+        character: 'tina'
+      },
+      {
+        title: 'curl + press',
+        img: 'https://media.giphy.com/media/Lq1ERaOKq2LEuK7xd1/giphy.gif',
+        character: 'spongebob'
+      },
+      {
+        title: 'lateral raise',
+        img: 'https://media.giphy.com/media/cKbb0WgweRU2GU15Cc/giphy.gif',
+        character: 'bob'
+      },
+      {
+        title: 'plank raise tap crunch',
+        img: 'https://media.giphy.com/media/ZDuizwB1eyoahVcakV/giphy.gif',
+        character: 'rick',
+        bodyweight: true,
+        unilateral: true
+      },
+      {
+        title: 'hammer curl',
+        img: 'https://media.giphy.com/media/izaYkYoJMnY07WYFzj/giphy.gif',
+        character: 'meeseeks'
+      },
+      {
+        title: 'pike pushup',
+        img: 'https://media.giphy.com/media/eLYEh1IRtaP7oApdCo/giphy.gif',
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'tricep dip',
+        img: 'https://media.giphy.com/media/W3emLNQKTecUA1POxp/giphy.gif',
+        character: 'spongebob',
+        bodyweight: true
+      },
+      {
+        title: 'tricep pushup',
+        img: 'https://media.giphy.com/media/KehD7SA2RF4gqHQP8r/giphy.gif',
+        character: 'meeseeks',
+        bodyweight: true
+      },
+      {
+        title: 'front shoulder raise',
+        img: 'https://media.giphy.com/media/fA2bpgpVfdJWgz1YZ9/giphy.gif',
+        character: 'bob'
+      },
+      {
+        title: 'shoulder press',
+        img: 'https://media.giphy.com/media/SUtRTnS36VFpC12IrT/giphy.gif',
+        character: 'tina'
+      },
+      {
+        title: 'tricep kickback',
+        img: 'https://media.giphy.com/media/fvfoHSsvAeUVrrNc33/giphy.gif',
+        character: 'meeseeks'
+      },
+      {
+        title: 'plank tricep extension',
+        img: 'https://media.giphy.com/media/lQCdZTKL8NbiU4OViv/giphy.gif',
+        character: 'bob',
+        bodyweight: true
+      },
+      {
+        title: 'chest fly',
+        img: 'https://media.giphy.com/media/LOXDvvmMvnk33n12pC/giphy.gif',
+        character: 'tina'
+      },
+      {
+        title: 'pullover',
+        img: 'https://media.giphy.com/media/Lo03vYJTNH6ouIRr4V/giphy.gif',
+        character: 'bmo'
+      },
+      {
+        title: 'chest press',
+        img: 'https://media.giphy.com/media/d8FZU70Rkj1LGIImBB/giphy.gif',
+        character: 'tina'
+      },
+      {
+        title: 'scarecrow raise',
+        img: 'https://media.giphy.com/media/JRUrE0VJcIifpGOxpy/giphy.gif',
+        character: 'bob',
+      },
+      {
+        title: 'burpee',
+        img: 'https://media.giphy.com/media/RlrSX7HvlDddz4Qmcc/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'reverse fly',
+        img: 'https://media.giphy.com/media/kfvD2clhN8AaM7P4L5/giphy.gif',
+        character: 'tina'
+      },
+      {
+        title: 'fly rotation',
+        img: 'https://media.giphy.com/media/Y0UMEAVeLtIAgmOuZA/giphy.gif',
+        character: 'bmo'
+      },
+      {
+        title: 'wide grip pushup',
+        img: 'https://media.giphy.com/media/iFt57qzEXZisX6JcZ7/giphy.gif',
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'power snatch',
+        img: 'https://media.giphy.com/media/eLv4iOPVrSI0PoNA1Z/giphy.gif',
+        character: 'prince',
+        unilateral: true
+      },
+      {
+        title: 'overhead tricep extension',
+        img: 'https://media.giphy.com/media/QAPISHGDiqMr46KY34/giphy.gif',
+        character: 'bob'
+      },
+  
+      {
+        title: 'forearm plank to dolphin',
+        img: 'https://media.giphy.com/media/lnxh6bTaaZeApIlH2j/giphy.gif',
+        character: 'rick',
+        bodyweight: true
+      }
+    ],
+    legs: [
+      {
+        title: 'pistol squat',
+        img: 'https://media.giphy.com/media/hqU0p2ivboCEYJQJaM/giphy.gif',
+        character: 'rick',
+        bodyweight: true,
+        unilateral: true
+      },
+      {
+        title: 'jumping jack',
+        img: 'https://media.giphy.com/media/lNWbCKbffqCIzY8RR1/giphy.gif',
+        character: 'poopybutthole',
+        bodyweight: true
+      },
+      {
+        title: 'calf raise',
+        img: 'https://media.giphy.com/media/QyhLeRDWoIusTLMyNk/giphy.gif',
+        character: 'bmo',
+        bodyweight: true
+      },
+      {
+        title: 'wall sit',
+        img: 'https://media.giphy.com/media/iDHzTcVMzTq6zaxhL4/giphy.gif',
+        character: 'rick',
+        bodyweight: true
+      },
+      {
+        title: 'squat',
+        img: 'https://media.giphy.com/media/XEZsDl0zybBbwJQV07/giphy.gif',
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'reverse lunge',
+        img: 'https://media.giphy.com/media/QWR3SDoEL6ggzL9rub/giphy.gif',
+        character: 'meeseeks',
+        bodyweight: true,
+        unilateral: true
+      },
+      {
+        title: 'side lunge',
+        img: 'https://media.giphy.com/media/h8glgzspdY8A0eNh1D/giphy.gif',
+        character: 'rick',
+        bodyweight: true,
+        unilateral: true
+      },
+      {
+        title: 'jumprope',
+        img: 'https://media.giphy.com/media/Y1jNTxApOQRfKOJt8k/giphy.gif',
+        character: 'bmo',
+        bodyweight: true
+      },
+      {
+        title: 'bulgarian split squat',
+        img: 'https://media.giphy.com/media/YqhMwl3nSuA2Dr3i5v/giphy.gif',
+        unilateral: true,
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'high knee',
+        img: 'https://media.giphy.com/media/W089J4JifogF6mba9y/giphy.gif',
+        character: 'rick',
+        bodyweight: true
+      },
+      {
+        title: 'squat jump',
+        img: 'https://media.giphy.com/media/dWsKZW6A3PdJnKROMl/giphy.gif',
+        character: 'meeseeks',
+        bodyweight: true
+      },
+      {
+        title: 'squat to kneel',
+        img: 'https://media.giphy.com/media/ZbN2PwMLD0boIjJrrn/giphy.gif',
+        character: 'bmo',
+        bodyweight: true
+      },
+      {
+        title: 'inner thigh leg raise',
+        img: 'https://media.giphy.com/media/Y1v6Ohkohe4yjM2F4r/giphy.gif',
+        character: 'bob',
+        bodyweight: true,
+        unilateral: true
+      },
+      {
+        title: 'curtsy lunge',
+        img: 'https://media.giphy.com/media/SqqsM3NXDgY1K1l7pt/giphy.gif',
+        character: 'rick',
+        bodyweight: true,
+        unilateral: true
+      }
+    ],
+    butt: [
+      {
+        title: 'squat to leg raise',
+        img: 'https://media.giphy.com/media/SwNf4vey9bnBLRypko/giphy.gif',
+        character: 'bmo',
+        bodyweight: true,
+        unilateral: true
+      },
+      {
+        title: 'glute kick back',
+        img: 'https://media.giphy.com/media/JPy2RNHCXbbLGw59pV/giphy.gif',
+        unilateral: true,
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'glute bridge',
+        img: 'https://media.giphy.com/media/cKKAg0wrGRYENJiUei/giphy.gif',
+        character: 'poopybutthole',
+        bodyweight: true
+      },
+      {
+        title: 'deadlift',
+        img: 'https://media.giphy.com/media/Yqz7ZokQ0kCs8uaN9E/giphy.gif',
+        character: 'meeseeks'
+      },
+      {
+        title: 'glute bridge march',
+        img: 'https://media.giphy.com/media/KZTBAKLnPVrpTuYag8/giphy.gif',
+        character: 'bob',
+        bodyweight: true
+      },
+      {
+        title: 'single leg glute bridge',
+        img: 'https://media.giphy.com/media/ZG16HjoxNrCApq0xg3/giphy.gif',
+        character: 'rick',
+        bodyweight: true,
+        unilateral: true
+      },
+      {
+        title: 'fire hydrant',
+        img: 'https://media.giphy.com/media/YMY5NMCtfOBJXGAtXY/giphy.gif',
+        unilateral: true,
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'sumo squat',
+        img: 'https://media.giphy.com/media/emGfboB1RYoSodN7XG/giphy.gif',
+        character: 'meeseeks',
+        bodyweight: true
+      },
+      {
+        title: 'glute bridge walk out',
+        img: 'https://media.giphy.com/media/cir6FkDe7MXgmffNcd/giphy.gif',
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'lunge to knee raise',
+        img: 'https://media.giphy.com/media/YqnrLFFt8KVfJ7Y09Q/giphy.gif',
+        character: 'bmo',
+        unilateral: true,
+        bodyweight: true
+      },
+      {
+        title: 'squat to side crunch',
+        img: 'https://media.giphy.com/media/QvRsGfbid6t6YTp6bi/giphy.gif',
+        character: 'bob',
+        unilateral: true,
+        bodyweight: true
+      },
+      {
+        title: 'single leg deadlift',
+        img: 'https://media.giphy.com/media/YmhQcV7p3jabLZJZSt/giphy.gif',
+        character: 'prince',
+        unilateral: true,
+        bodyweight: true
+      },
+  
+  
+      {
+        title: 'frog pump',
+        img: 'https://media.giphy.com/media/MEjmyZ6dKVNBhZlXfq/giphy.gif',
+        character: 'rick',
+        bodyweight: true
+      }
+    ],
+    back: [
+      {
+        title: 'plank row',
+        img: 'https://media.giphy.com/media/l4M6n87ScoBRUrjMBD/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'superman',
+        img: 'https://media.giphy.com/media/lTArkNAXA1Dk9b4PsX/giphy.gif',
+        character: 'poopybutthole',
+        bodyweight: true
+      },
+      {
+        title: 'prone lat pulldown',
+        img: 'https://media.giphy.com/media/ZeKvP6fl0ZV63RlV8E/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'upright row',
+        img: 'https://media.giphy.com/media/h7tvdrSjSzmB3XUPgH/giphy.gif',
+        character: 'tina'
+      },
+  
+      {
+        title: 'reverse snow angel',
+        img: 'https://media.giphy.com/media/kzqvH8IO3cXqDEVT1y/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'i-t-y',
+        img: 'https://media.giphy.com/media/Wp0gT1ibDsmUU5KmHm/giphy.gif',
+        character: 'meeseeks'
+      },
+      {
+        title: 'crab walk',
+        img: 'https://media.giphy.com/media/jqGmd9gmGxDPXby3wc/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'row to tricep kickback',
+        img: 'https://media.giphy.com/media/fqbSg8AqQ09t09RYsY/giphy.gif',
+        unilateral: true,
+        character: 'bmo'
+      },
+      {
+        title: 'posterior plank',
+        img: 'https://media.giphy.com/media/Rkn765eSFkmJTS4Npe/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'row',
+        img: 'https://media.giphy.com/media/cmg3rpWI915jERDeYY/giphy.gif',
+        character: 'tina'
+      },
+      {
+        title: 'bird dog crunch',
+        img: 'https://media.giphy.com/media/eKCrBlcoEx2yIJFjPT/giphy.gif',
+        character: 'prince',
+        unilateral: true,
+        bodyweight: true
+      },
+      {
+        title: 'good morning',
+        img: 'https://media.giphy.com/media/UUngOy2VaiPCYrh2uq/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'plank reach to down dog toe touch',
+        img: 'https://media.giphy.com/media/L3WFDllHa0UJxS6Lyj/giphy.gif',
+        character: 'prince',
+        bodyweight: true,
+        unilateral: true
+      }
+    ],
+    core: [
+      {
+        title: 'commando',
+        img: 'https://media.giphy.com/media/XeMer2mEKQauNA6ISm/giphy.gif',
+        character: 'bob',
+        bodyweight: true
+      },
+      {
+        title: 'crab toe touch',
+        img: 'https://media.giphy.com/media/JUejec6cI22RSbM62j/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'bicycle crunch',
+        img: 'https://media.giphy.com/media/cKmbHxWIQrs5x4k6ur/giphy.gif',
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'plank jack',
+        img: 'https://media.giphy.com/media/kegbiTdPMMqUzWb4ub/giphy.gif',
+        character: 'meeseeks',
+        bodyweight: true
+      },
+      {
+        title: 'standing side crunch',
+        img: 'https://media.giphy.com/media/Q8a7nGMti3uuCFHfF5/giphy.gif',
+        unilateral: true,
+        character: 'tina'
+      },
+      {
+        title: 'plank shoulder tap',
+        img: 'https://media.giphy.com/media/ejCVKLqCrppg2TpFgp/giphy.gif',
+        character: 'bob',
+        bodyweight: true
+      },
+      {
+        title: 'side plank',
+        img: 'https://media.giphy.com/media/jq0G0EmNEGYyfhENhA/giphy.gif',
+        unilateral: true,
+        character: 'bmo',
+        bodyweight: true
+      },
+      {
+        title: 'bear plank',
+        img: 'https://media.giphy.com/media/dscqPpbt6z16WJSvkc/giphy.gif',
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'dead bug',
+        img: 'https://media.giphy.com/media/Ib19iaeZvif1be0rbL/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'mountain climber',
+        img: 'https://media.giphy.com/media/W35qtGrrpweU00eLwI/giphy.gif',
+        character: 'meeseeks',
+        bodyweight: true
+      },
+      {
+        title: 'sit up',
+        img: 'https://media.giphy.com/media/TiCqf0dCsS0UiSLWqd/giphy.gif',
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'russian twist',
+        img: 'https://media.giphy.com/media/jt3y986EVb6L1woQS6/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'forearm plank',
+        img: 'https://media.giphy.com/media/hXO58SRA8RSj2cfgge/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'plank',
+        img: 'https://media.giphy.com/media/J2rGWkLU7IS5D2Us7f/giphy.gif',
+        character: 'bmo',
+        bodyweight: true
+      },
+      {
+        title: 'crunch',
+        img: 'https://media.giphy.com/media/J5dtNyZYvFIFFC1fUV/giphy.gif',
+        character: 'tina',
+        bodyweight: true
+      },
+      {
+        title: 'toe touch',
+        img: 'https://media.giphy.com/media/UoeJ3obguCjCR4nisU/giphy.gif',
+        character: 'prince',
+        bodyweight: true
+      },
+      {
+        title: 'forearm plank knee to elbow',
+        img: 'https://media.giphy.com/media/elhRhEtr2uYqXuDUfW/giphy.gif',
+        character: 'bob',
+        bodyweight: true,
+        unilateral: true
+      }
+    ]
+  }
