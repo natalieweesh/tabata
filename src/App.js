f17071760bf25d71686b9f63e6fd95116f71b599
Natalie Wee <natalieweesh@gmail.com>
2020-05-27 09:08:18 -0400

    one more mobile styling fix

--- src/App.js
+++ src/App.js
@@ -86,10 +86,12 @@ function App() {
       let newRow = <tr><td>{trainer}</td><td>{trainerExercises}</td></tr>;
       trainerTableRows.push(newRow)
     })
-    return <div className='row statisticsRow'>
+    return <div>
       <p className='exerciseTitle'>that's {allExercises.length} different exercises for you to do!</p>
-      <table className='column'><tbody><tr><td>muscle<br></br>group</td><td>total<br/>exercises</td><td>bodyweight<br/>exercises</td><td>low impact<br/>exercises</td></tr>{tableRows.map(e => e)}</tbody></table>
-      <table className='column '><tbody><tr><td>trainer</td><td>total<br/>exercises</td></tr>{trainerTableRows.map(e => e)}</tbody></table>
+      <div className='row statisticsRow'>
+        <table className='column'><tbody><tr><td>muscle<br></br>group</td><td>total<br/>exercises</td><td>bodyweight<br/>exercises</td><td>low impact<br/>exercises</td></tr>{tableRows.map(e => e)}</tbody></table>
+        <table className='column '><tbody><tr><td>trainer</td><td>total<br/>exercises</td></tr>{trainerTableRows.map(e => e)}</tbody></table>
+      </div>
     </div>
   }
 
